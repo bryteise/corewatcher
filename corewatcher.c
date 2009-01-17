076258ddddcdb72c7d824f3fa1c55916063a91e5
Arjan van de Ven <arjan@linux.intel.com>
2009-01-17 16:56:23 +0200

    add author

--- corewatcher.c
+++ corewatcher.c
@@ -3,6 +3,9 @@
  *
  * (C) 2009 Intel Corporation
  *
+ * Authors:
+ * 	Arjan van de Ven <arjan@linux.intel.com>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License.
