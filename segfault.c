e7ff58383ef58cc5828e7f29c79f484c0cd3147c
Auke Kok <auke-jan.h.kok@intel.com>
2011-02-02 14:31:20 -0500

    disclaimer.

--- segfault.c
+++ segfault.c
@@ -2,6 +2,11 @@
  * segfault.c - a program that is intended to create a segmentation
  *              fault (SIGSEGV).
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  That also
+ * implies that it might not even segfault in the first place.
+ *
  * No license - this code originates from Public Domain examples
  * on the internet. Not that I'd want to claim copyright anyway
  * on a program that is intended to crash in the first place.
